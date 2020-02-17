5e4f89c4a906b9bb7374a43810f2abd598a314b1
User <1>
2020-02-17 04:00:17 +0300

    Commit code

--- src/test/java/BmiCalculatorTest.java
+++ src/test/java/BmiCalculatorTest.java
@@ -8,15 +8,39 @@ import static org.testng.Assert.assertEquals;
 public class BmiCalculatorTest {
 
     @Test
-    public void categoryTest() {
-        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+    public void starvationTest() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver browser = new ChromeDriver();
         browser.get("https://healthunify.com/bmicalculator/");
-        browser.findElement(By.name("wg")).sendKeys("93");
-        browser.findElement(By.name("ht")).sendKeys("178");
+        browser.findElement(By.name("wg")).sendKeys("10.1");
+        browser.findElement(By.name("ht")).sendKeys("100");
         browser.findElement(By.name("cc")).click();
         String category = browser.findElement(By.name("desc")).getAttribute("value");
-        assertEquals(category,"Your category is Overweight","Категория неверная");
+        assertEquals(category, "Your category is Starvation", "Категория неверная");
+        browser.quit();
+    }
+
+    @Test
+    public void underweightTest() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://healthunify.com/bmicalculator/");
+        browser.findElement(By.name("wg")).sendKeys("15.1");
+        browser.findElement(By.name("ht")).sendKeys("100");
+        browser.findElement(By.name("cc")).click();
+        String category = browser.findElement(By.name("desc")).getAttribute("value");
+        assertEquals(category, "Your category is Underweight", "Категория неверная");
+        browser.quit();
+    }
+
+    @Test
+    public void alertTest() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://healthunify.com/bmicalculator/");
+        browser.findElement(By.name("wg")).sendKeys("11");
+        browser.findElement(By.name("cc")).click();
+        browser.switchTo().alert().accept();
         browser.quit();
     }
 }
